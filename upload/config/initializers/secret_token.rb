bb1a4eaa6dad1da8c103cc26ea031dea8781c4db
kazuya tanizawa <kazuyat7175@gmail.com>
2012-02-27 08:26:24 +0900

    first commit

---
+++ upload/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Upload::Application.config.secret_token = '0e2139ffe3fbe7c305cda8cbac8de08b994052ac9d33c2f51dfb3a6abf48570c719b2d6e238343e27d0176ee8aa9c4bb8998ab231f38b075e910aeb266b39fb7'
